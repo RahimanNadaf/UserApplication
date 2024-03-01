2f52d413345d821bf373825868294412648be6e0
Rahiman <rahimannadaf198@gmail.com>
2024-03-01 19:53:51 +0530

    FIx unit test cases

--- UserManagement.Web.Tests/UserControllerTests.cs
+++ UserManagement.Web.Tests/UserControllerTests.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Hosting;
 using UserManagement.Models;
 using UserManagement.Services.Domain.Interfaces;
 using UserManagement.Web.Models.Users;
@@ -44,5 +45,7 @@ public class UserControllerTests
     }
 
     private readonly Mock<IUserService> _userService = new();
-    private UsersController CreateController() => new(_userService.Object);
+
+    private readonly Mock<IWebHostEnvironment> _webHostEnvironment = new Mock<IWebHostEnvironment>();
+    private UsersController CreateController() => new(_userService.Object,_webHostEnvironment.Object);
 }
